dea7dd9601131493701acdc25847132a4699d02f
goodguytim1 <timobrien56@knights.ucf.edu>
2020-10-12 21:53:31 -0400

    Blog Commit 2

--- exercise/app.js
+++ exercise/app.js
@@ -1,18 +1,40 @@
 const express = require("express");
 const app = express();
-const Post = require("./api/models/post");
+const multer  = require('multer');
+const Post = require('./api/models/post');
 const postsData = new Post();
 
-app.use((req, res, next)=> {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`)
+    }
+});
+
+const getExt = (mimetype) => {
+    switch(mimetype){
+        case "image/png":
+            return '.png';
+        case "image/jpeg":
+            return '.jpg';
+    }
+}
+
+var upload = multer({ storage: storage });
+
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     next();
 });
-
+app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
-app.get("/api/posts/", (req, res) => {
-    res.status(200).send(postsData.get())
-});
+app.get("/api/posts", (req, res)=>{
+    res.status(200).send(postsData.get());
+})
 
 app.get("/api/posts/:postId", (req, res)=>{
     const postId = req.params.postId;
@@ -25,4 +47,18 @@ app.get("/api/posts/:postId", (req, res)=>{
     }
 })
 
-app.listen(3000, () => console.log("Listening on http://localhost:3000"));
+app.post("/api/posts", upload.single("post-image") ,(req, res)=>{
+    var postImage = req.file.path;
+    var index = postImage.replace('\\', '/');
+    const newPost = {
+        "id": `${Date.now()}`,
+        "title": req.body.title,
+        "content": req.body.content,
+        "post_image": req.file.path.replace('\\', '/'),
+        "added_date": `${Date.now()}`
+    }
+    postsData.add(newPost);
+    res.status(201).send(postImage);
+})
+
+app.listen(3000, ()=>console.log("Listening on http://localhost:3000/"));
